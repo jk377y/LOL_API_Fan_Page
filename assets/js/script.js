3d6c343f74355609a5154b5ca2e3c2643047a765
James Kelly <jk377y@gmail.com>
2022-12-27 02:37:05 -0600

    fully destructured LOL api object, ready for next step to appendchild data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,34 +1,39 @@
 let leagueOfLegendsAPI = "http://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/champion.json"
 
 fetch(leagueOfLegendsAPI)
-    .then(response => {
-        // console.log(response);
-        return response.json()
-        
-    }).then (data => {
-        
-        let champArray = Object.keys(data.data)
-        for(let i = 0; i < champArray.length; i++) {
-            let champIndex = champArray[i]
-            let firstAttempt = (data.data[champIndex])
-            let splashImage = (data.data[champIndex].image.full)
-            let splashImageUrl = "http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/"+splashImage
-            // console.log(champArray);
-            //console.log(champIndex);
-            console.log(firstAttempt, splashImageUrl);
-            // console.log(firstAttempt);
-        }
-
-        // console.log(champArray[2]);
-       
-        
-        
-        // 
-        // 
-        // let secondAttempt = 
-        // console.log(firstAttempt);
-        // console.log(champIndex);
-        // console.log(secondAttempt);
-        // }
-    })
+    .then(response => response.json())
+    .then(data => {
+    let champArray = Object.keys(data.data)   // creates array of the first key values in the data response object
+    for(let i = 0; i < champArray.length; i++) {
+        let champIndex = champArray[i];      //  array of just champion names
+        let allChampObjects = data.data[champIndex];  // returns all 162 champion objects
+        let {data: 
+                {[champIndex]: {
+                    blurb,
+                    id,
+                    image: {full, group, sprite},
+                    info: {attack, defense, difficulty, magic},
+                    key,
+                    name,
+                    partype,
+                    stats: {armor, armorperlevel, attackdamage, attackdamageperlevel, attackrange, attackspeed, attackspeedperlevel, crit, critperlevel, hp, hpperlevel, hpregen, hpregenperlevel, movespeed, mp, mpperlevel, mpregen, mpregenperlevel, spellblock, spellblockperlevel},
+                    tags: {[0]: role1, [1]: role2},
+                    title
+                }}
+                
+            } = data;   // fully destructures each champion 
+            console.log(allChampObjects);    
+    }});
     
+    const menuBtn = document.querySelector('.hamburger');
+    const mobile_menu = document.querySelector('.mobileNav')
+    menuBtn.addEventListener('click', function () {
+        menuBtn.classList.toggle('is-active');
+        mobile_menu.classList.toggle('is-active');
+    });
+
+
+
+    // let splashImage = (data.data[champIndex].image.full)
+    // let splashImageUrl = "http://ddragon.leagueoflegends.com/cdn/12.23.1/img/champion/"+splashImage
+            
