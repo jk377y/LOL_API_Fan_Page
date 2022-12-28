ab158e12d78d7142592a173554de678f8fa7ff24
James Kelly <jk377y@gmail.com>
2022-12-28 10:10:13 -0600

    completed thumbnail gallery on champions page

--- assets/js/champion.js
+++ assets/js/champion.js
@@ -36,7 +36,7 @@ fetch(leagueOfLegendsAPI)
                 }}
                 
             } = data;   // fully destructures each champion 
-            
+            console.log(data.data);
             //document.querySelector('#blurb').textContent = blurb
             //document.querySelector('#id').textContent = id  // sets value to zyra after the 162 loop
             //document.querySelector('#full').textContent = full
@@ -77,33 +77,128 @@ fetch(leagueOfLegendsAPI)
             
             const pBlurbEl = document.createElement('p');
             pBlurbEl.textContent = blurb
-            pBlurbEl.classList.add('white','blurb');
+            pBlurbEl.classList.add('white','blurb','hide');
             champDataDiv.appendChild(pBlurbEl);
         
             const pIdEl = document.createElement('p');
             pIdEl.textContent = id
-            pIdEl.classList.add('white','id');
+            pIdEl.classList.add('white','id','hide');
             champDataDiv.appendChild(pIdEl);
             
             const pgroupEl = document.createElement('p');
             pgroupEl.textContent = group
-            pgroupEl.classList.add('white','group');
+            pgroupEl.classList.add('white','group','hide');
             champDataDiv.appendChild(pgroupEl);
             
             const pSpriteEl = document.createElement('p');
             pSpriteEl.textContent = sprite
-            pSpriteEl.classList.add('white','sprite');
+            pSpriteEl.classList.add('white','sprite','hide');
             champDataDiv.appendChild(pSpriteEl);
 
             const pAttackEl = document.createElement('p');
             pAttackEl.textContent = attack
-            pAttackEl.classList.add('white','attack');
+            pAttackEl.classList.add('white','attack','hide');
             champDataDiv.appendChild(pAttackEl);
             
             const pDefenseEl = document.createElement('p');
             pDefenseEl.textContent = defense
-            pDefenseEl.classList.add('white','defense');
+            pDefenseEl.classList.add('white','defense','hide');
             champDataDiv.appendChild(pDefenseEl);
+
+            const pDifficultyEl = document.createElement('p');
+            pDifficultyEl.textContent = difficulty
+            pDifficultyEl.classList.add('white','difficulty','hide');
+            champDataDiv.appendChild(pDifficultyEl);
+
+            const pMagicEl = document.createElement('p');
+            pMagicEl.textContent = magic
+            pMagicEl.classList.add('white','magic','hide');
+            champDataDiv.appendChild(pMagicEl);
+
+            const pKeyEl = document.createElement('p');
+            pKeyEl.textContent = key
+            pKeyEl.classList.add('white','key','hide');
+            champDataDiv.appendChild(pKeyEl);
+
+            const pNameEl = document.createElement('p');
+            pNameEl.textContent = name
+            pNameEl.classList.add('white','name','hide');
+            champDataDiv.appendChild(pNameEl);
+
+            const pPartypeEl = document.createElement('p');
+            pPartypeEl.textContent = partype
+            pPartypeEl.classList.add('white','partype','hide');
+            champDataDiv.appendChild(pPartypeEl);
+            
+            const pArmorEl = document.createElement('p');
+            pArmorEl.textContent = armor
+            pArmorEl.classList.add('white','armor','hide');
+            champDataDiv.appendChild(pArmorEl);
+
+            const pAttackDamageEl = document.createElement('p');
+            pAttackDamageEl.textContent = attackdamage
+            pAttackDamageEl.classList.add('white','attackdamage','hide');
+            champDataDiv.appendChild(pAttackDamageEl);
+
+            const pAttackRangeEl = document.createElement('p');
+            pAttackRangeEl.textContent = attackrange
+            pAttackRangeEl.classList.add('white','attackrange','hide');
+            champDataDiv.appendChild(pAttackRangeEl);
+
+            const pAttackSpeedEl = document.createElement('p');
+            pAttackSpeedEl.textContent = attackspeed
+            pAttackSpeedEl.classList.add('white','attackspeed','hide');
+            champDataDiv.appendChild(pAttackSpeedEl);
+
+            const pCritEl = document.createElement('p');
+            pCritEl.textContent = crit
+            pCritEl.classList.add('white','crit','hide');
+            champDataDiv.appendChild(pCritEl);
+
+            const pHpEl = document.createElement('p');
+            pHpEl.textContent = hp
+            pHpEl.classList.add('white','hp','hide');
+            champDataDiv.appendChild(pHpEl);
+
+            const pHpRegenEl = document.createElement('p');
+            pHpRegenEl.textContent = hpregen
+            pHpRegenEl.classList.add('white','hpregen','hide');
+            champDataDiv.appendChild(pHpRegenEl);
+
+            const pMpEl = document.createElement('p');
+            pMpEl.textContent = mp
+            pMpEl.classList.add('white','mp','hide');
+            champDataDiv.appendChild(pMpEl);
+
+            const pMpRegenEl = document.createElement('p');
+            pMpRegenEl.textContent = mpregen
+            pMpRegenEl.classList.add('white','mpregen','hide');
+            champDataDiv.appendChild(pMpRegenEl);
+
+            const pMoveSpeedEl = document.createElement('p');
+            pMoveSpeedEl.textContent = movespeed
+            pMoveSpeedEl.classList.add('white','movespeed','hide');
+            champDataDiv.appendChild(pMoveSpeedEl);
+
+            const pSpellBlockEl = document.createElement('p');
+            pSpellBlockEl.textContent = spellblock
+            pSpellBlockEl.classList.add('white','spellblock','hide');
+            champDataDiv.appendChild(pSpellBlockEl);
+
+            const pRole1El = document.createElement('p');
+            pRole1El.textContent = role1
+            pRole1El.classList.add('white','role1','hide');
+            champDataDiv.appendChild(pRole1El);
+
+            const pRole2El = document.createElement('p');
+            pRole2El.textContent = role2
+            pRole2El.classList.add('white','role2','hide');
+            champDataDiv.appendChild(pRole2El);
+
+            const pTitleEl = document.createElement('p');
+            pTitleEl.textContent = title
+            pTitleEl.classList.add('white','title','hide');
+            champDataDiv.appendChild(pTitleEl);
 
             gallery.appendChild(champDataDiv);
             //console.log(pBlurbEl);
